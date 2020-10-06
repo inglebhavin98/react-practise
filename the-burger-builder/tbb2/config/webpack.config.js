018dbb2809679463358e55cb091305ddec68928e
inglebhavin98 <inglebhavin98@gmail.com>
2020-10-06 22:49:17 +0530

    Update webpack.config.js
    Stable commit. Modified webpack.

--- the-burger-builder/tbb2/config/webpack.config.js
+++ the-burger-builder/tbb2/config/webpack.config.js
@@ -432,7 +432,7 @@ module.exports = function(webpackEnv) {
               use: getStyleLoaders({
                 importLoaders: 1,
                 sourceMap: isEnvProduction && shouldUseSourceMap,
-              }),
+              }), // success
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
               // Remove this when webpack adds a warning or an error for this.
