631ec81ebcc99abed3118661c228b25f6fc659e4
inglebhavin98 <inglebhavin98@gmail.com>
2020-10-13 17:29:35 +0530

    130 Ingredients sum
    Check documentation of map() & reduce()

--- the-burger-builder/tbb2/src/components/Burger/Burger.js
+++ the-burger-builder/tbb2/src/components/Burger/Burger.js
@@ -9,13 +9,20 @@ const burger = (props) => {
     // keys->array->properties->map->(gives keys to strings)->string->array->no of elements(1,2)elements
     // Array-object->create array->amount of given ingredient
     // _ blank element, 
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
     .map( igKey => {
         return [...Array(props.ingredients[igKey])].map((_,i)=>{
            return <BurgerIngredient key={igKey + i} type={igKey}/>
 
         });
-    });
+    })
+    .reduce((arr,el)=>{
+        return arr.concat(el)
+    },[]);
+    if (transformedIngredients.length===0){
+        transformedIngredients = <p>Please start adding Ingredients!</p>
+    }
+
 
     return (
         <div className={classes.Burger}>
