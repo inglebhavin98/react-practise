bb6dcb4d80febd06e37d4c20f62e0c3fc17767c3
inglebhavin98 <inglebhavin98@gmail.com>
2020-10-13 15:08:04 +0530

    129 Dynamic content - state
    using state
    internal map method - did not understand clearly
    debug/understand by console logging to understand better
    passed props to child elements using this.state.
    
    read more on this & go thru

--- the-burger-builder/tbb2/src/components/Burger/Burger.js
+++ the-burger-builder/tbb2/src/components/Burger/Burger.js
@@ -4,12 +4,23 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredient';
 
 const burger = (props) => {
+    // new constant 
+    // object - default js -> keys method -> array of keys -> "cheese" "meat"..
+    // keys->array->properties->map->(gives keys to strings)->string->array->no of elements(1,2)elements
+    // Array-object->create array->amount of given ingredient
+    // _ blank element, 
+    const transformedIngredients = Object.keys(props.ingredients)
+    .map( igKey => {
+        return [...Array(props.ingredients[igKey])].map((_,i)=>{
+           return <BurgerIngredient key={igKey + i} type={igKey}/>
+
+        });
+    });
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
-            <BurgerIngredient type="meat"/>
+             {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
