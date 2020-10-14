5117a9c0afeafa8d5eb5325a73ac9f8ba2de6714
inglebhavin98 <inglebhavin98@gmail.com>
2020-10-14 18:48:00 +0530

    158. The Burger Builder Basic Version Final
    148. responsive adjustments
    149. reusing backdrop
    improvements
    - prop types
    - pure components
    
    console log & see when and how willupdate() didupdate() etc functions are called, so that they are not called when not required and cause trouble...

--- the-burger-builder/tbb2/src/components/Navigation/Toolbar/Toolbar.js
+++ the-burger-builder/tbb2/src/components/Navigation/Toolbar/Toolbar.js
@@ -2,16 +2,18 @@ import React from 'react';
 
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
-import NavItems from '../../Navigation/NavigationItems/NavItems'
+import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
-const toolbar =() =>(
+const toolbar = ( props ) => (
     <header className={classes.Toolbar}>
-        <div>Menu</div>
-        <Logo/>
-        <nav>
-            <NavItems/>
+        <DrawerToggle clicked={props.drawerToggleClicked} />
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
+            <NavigationItems />
         </nav>
-
     </header>
 );
 
