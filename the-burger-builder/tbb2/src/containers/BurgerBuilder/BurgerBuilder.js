c21d66622498ae3b7bdef7c017ba1679823ac7fe
inglebhavin98 <inglebhavin98@gmail.com>
2020-10-14 13:17:37 +0530

    139. Backdrop component
    How component is added at the bottom and integrated step by step to the top most component.

--- the-burger-builder/tbb2/src/containers/BurgerBuilder/BurgerBuilder.js
+++ the-burger-builder/tbb2/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -70,6 +70,12 @@ class BurgerBuilder extends Component{
     purchaseHandler = () => {
         this.setState({purchasing:true});
     }
+
+    purchaseCancelHandler = () => {
+        this.setState({purchasing:false});
+    }
+
+
     render(){
         const disabledInfo ={
             ...this.state.ingredients
@@ -80,7 +86,7 @@ class BurgerBuilder extends Component{
         // salad:true, meat:false...
         return (
             <Aux>
-                <Modal show={this.state.purchasing}>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary ingredients={this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
