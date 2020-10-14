ef809170d348958d50368b0a1e7ff823cd79eb4e
inglebhavin98 <inglebhavin98@gmail.com>
2020-10-14 11:43:57 +0530

    137. Order summary Modal
    - why uses Object.keys
    - checkout code for tcp & why they use what they use
    
    how we import components & pass things to the imported things
    
    Use of KEY in elements

--- the-burger-builder/tbb2/src/containers/BurgerBuilder/BurgerBuilder.js
+++ the-burger-builder/tbb2/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,7 +3,8 @@ import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
-
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -30,9 +31,9 @@ class BurgerBuilder extends Component{
         .map(igKey => {
             return ingredients[igKey];
         })
-        .reduce((sum,el)=> {
-            return sum +el;
-        },0);
+        .reduce(function (sum, el) {
+                return sum + el;
+            },0);
         this.setState({purchaseable:sum>0});
     }
 
@@ -75,6 +76,9 @@ class BurgerBuilder extends Component{
         // salad:true, meat:false...
         return (
             <Aux>
+                <Modal>
+                    <OrderSummary ingredients={this.state.ingredients}/>
+                </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
